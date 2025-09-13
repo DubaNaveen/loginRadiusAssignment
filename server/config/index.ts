a483128fc9fabb0396a63002d088309fdf7c95b4
Duba Naveen <dubanaveen19@gmail.com>
2025-09-13 14:11:57 +0530

    db credentials

--- server/config/index.ts
+++ server/config/index.ts
@@ -2,19 +2,20 @@ import dotenv from "dotenv";
 dotenv.config();
 export default {
   MYSQLDB: {
-    // DATABASE_NAME:process.env.DATABASE_NAME,
-    // USERNAME:process.env.USERNAME,
-    // PASSWORD:process.env.PASSWORD,
-    // HOST:process.env.HOST,
+    DATABASE_NAME:process.env.DATABASE_NAME,
+    USERNAME:process.env.DB_USERNAME,
+    PASSWORD:process.env.DB_PASSWORD,
+    HOST:process.env.DB_HOST,
+    PORT:Number(process.env.DB_PORT)
     // DATABASE_NAME: "loginRadius",
     // USERNAME: "root",
     // PASSWORD: "password",
     // HOST: "localhost",
-    DATABASE_NAME: "defaultdb",
-    USERNAME: "avnadmin",
-    PASSWORD: "***REMOVED***",
-    HOST: "mysql-29cc4346-dubanaveen19-2a29.k.aivencloud.com",
-    PORT: 20180
+    // DATABASE_NAME: "defaultdb",
+    // USERNAME: "avnadmin",
+    // PASSWORD: "AVNS_qkCecjZlR5i4tVsbFqI",
+    // HOST: "mysql-29cc4346-dubanaveen19-2a29.k.aivencloud.com",
+    // PORT: 20180
 
   },
   RUNNING_PORT: parseInt(process.env.PORT || "3304"),
